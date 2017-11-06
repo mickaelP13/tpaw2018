031eb397e991385db337d5b2e3819a486897135c
mickaelP13 <32840008+mickaelP13@users.noreply.github.com>
2017-11-06 16:56:29 +0100

    Add files via upload

---
+++ tp2/form-bs.js
@@ -0,0 +1,20 @@
+function valider(){
+	var nom = document.getElementById("nom").value;
+	var prenom = document.getElementById("prenom").value;
+	var adresse = document.getElementById("adresse").value;
+	var mail = document.getElementById("mail").value;
+	if (nom.length<5 || prenom.length<5 || adresse.length<5 || mail.length <5 )
+	{
+		document.getElementById("#error").style.visibility="visible";
+		document.getElementById("#resultat").style.visibility="hidden";
+		document.getElementById("error").innerHTML = "les champs doivent contenir 5 caracteres";
+	
+	}
+
+	else{
+		document.querySelector("#error").style.visibility="hidden";
+	document.querySelector("#resultat").style.visibility="visible";
+	document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;
+	
+}
+}
